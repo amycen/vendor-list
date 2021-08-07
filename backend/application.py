17b488139ca39c310dfce00b65c8da19d4aa2e78
Amy <cenamy@gmail.com>
2021-08-07 15:53:12 -0400

    fix: fix sys path error

--- backend/application.py
+++ backend/application.py
@@ -1,5 +1,6 @@
 import sys, os
-sys.path.append(sys.path.append(os.path.dirname(__file__)))
+# sys.path.append(sys.path.append(os.path.dirname(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from flask import Flask, render_template
 
